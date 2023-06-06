7ece0bab4831bd1b3b869ee973eeaf3fa6b2b238
Jonathan Barkei <100424922+Jonbarkei@users.noreply.github.com>
2023-06-05 21:14:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  const output = document.getElementById("output");
-  const commandInput = document.getElementById("command");
-
-  commandInput.addEventListener("keydown", function(e) {
-    if (e.key === "Enter") {
-      const command = commandInput.value;
-      commandInput.value = "";
-      executeCommand(command);
-    }
-  });
- output.innerHTML += "HEllO, I AM JON'S HELPER.\n";
-  function executeCommand(command) {
-    // Your logic for executing commands goes here
-    // For this example, let's just echo the entered command
-    output.innerHTML += "> " + command + "\n";
-    function executeCommand(command) {
+function executeCommand(command) {
   output.innerHTML += "> " + command + "\n";
   
   // Check for specific commands
-  if (command.startsWith("hi")) {
-      output.innerHTML += "HELLO.\n";
+  if (command.startsWith("weather")) {
+    const location = command.split(" ")[1];
+    if (location) {
+      getWeather(location);
+    } else {
+      output.innerHTML += "Please provide a location.\n";
     }
   } else {
     output.innerHTML += "Command not recognized.\n";
   }
 }
 
-  }
-});
-
+function getWeather(location) {
+  const apiKey = "YOUR_API_KEY";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const weather = data.weather[0].description;
+      const temp = Math.round(data.main.temp - 273.15); // Convert temperature to Celsius
+      output.innerHTML += `Weather in ${location}: ${weather}, Temperature: ${temp}°C\n`;
+    })
+    .catch(error => {
+      output.innerHTML += `Error fetching weather data: ${error}\n`;
+    });
+}
